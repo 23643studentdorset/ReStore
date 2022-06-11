d34414ab211e208115b34809b97170bd342ec673
23643studentdorset <23643@student.dorset-college.ie>
2022-06-11 07:06:31 +0100

    call to products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([
+    {name: 'Product1', price: 100.00},
+    {name: 'Product2', price: 200.00},
+  ]);
+
+  useEffect(() =>{
+    fetch('http://localhost:5000/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  }, [])
+
+  function addproduct() {
+    setProducts(prevState => [...products, {name: 'Product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+  }
+
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+      <button onClick ={addproduct}>Add product</button>
     </div>
   );
 }
