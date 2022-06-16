853084415c868031283cd67eb501ac79a4f10092
23643studentdorset <23643@student.dorset-college.ie>
2022-06-16 10:34:12 +0100

    not loading the pages

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,11 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/Contactpage";
+import HomePage from "../../features/home/Home";
 import Header from "./Header";
 
 function App() {
@@ -24,7 +29,11 @@ function handleThemeChange(){
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}  />
       <Container>
-        <Catalog/>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/catalog' component={Catalog}/>
+        <Route exact path='/catalog/:id' component={ProductDetails}/>
+        <Route exact path='/about' component={AboutPage}/>
+        <Route exact path='/contact' component={ContactPage}/>
       </Container>
     </ThemeProvider>
   );
