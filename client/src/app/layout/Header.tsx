380b4df343c1fa5aea82710b3aee97dd4c2b9da3
23643studentdorset <23643@student.dorset-college.ie>
2022-06-16 22:11:28 +0100

    .

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,5 @@
-import { AppBar, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 interface Props {
@@ -22,7 +23,7 @@ export default function Header({darkMode, handleThemeChange}:Props){
     return(
         <AppBar position = 'static' sx= {{mb : 4}}>
             <Toolbar>
-                <Typography variant= 'h6'>
+                <Typography variant= 'h6' component={NavLink} to='/' sx={{color:'inherit', textDecoration:'none'}}>
                     RE-STORE
                 </Typography>
                 <Switch checked={darkMode} onChange={handleThemeChange}/> 
@@ -38,6 +39,11 @@ export default function Header({darkMode, handleThemeChange}:Props){
                         </ListItem>
                     ))}
                 </List>
+                <IconButton size='large' sx={{color:'white', typography:'h6'}}>
+                    <Badge badgeContent={4} color='secondary'>
+                        <ShoppingCart />
+                    </Badge>
+                </IconButton>
                 <List sx={{display:'flex'}}>
                     {rightLinks.map(({title, path}) => (
                         <ListItem
